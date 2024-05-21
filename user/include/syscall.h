3b8a6403789b1f8a4f754822407b3fdfe7354464
LiShuang <2327168713@qq.com>
2024-05-21 12:18:50 +0800

    [user] 构建用户系统调用接口；

---
+++ user/include/syscall.h
@@ -0,0 +1,14 @@
+//
+// Created by Li Shuang ( pseudonym ) on 2024-05-21 
+// --------------------------------------------------------------
+// | Note: This code file just for study, not for commercial use 
+// | Contact Author: lishuang.mk@whu.edu.cn 
+// --------------------------------------------------------------
+//
+
+#ifndef _SYSCALL_H_
+#define _SYSCALL_H_
+
+extern int write( int fd, const void *buf, int len);
+
+#endif
--- user/user_init.c
+++ user/user_init.c
@@ -6,6 +6,8 @@
 // --------------------------------------------------------------
 //
 
+#include "syscall.h"
+
 char u_init_stack[ 2048 ] __attribute__( ( section( ".user.init.stack" ) ) );
 
 int init_main( void ) __attribute__( ( section( ".user.init" ) ) );
@@ -13,14 +15,15 @@ int init_main( void ) __attribute__( ( section( ".user.init" ) ) );
 int init_main( void )
 {
 	const char str[] = "Hello World\n";
-	asm volatile(
-		"li.d $a0, 1\n"
-		"add.d $a1, $zero, %0\n"
-		"li.d $a2, %1\n"
-		"li.d $a7, 64\n"
-		"syscall 0"
-		:: "r"( str ),
-		"i"( sizeof( str ) )
-		);
+	// asm volatile(
+	// 	"li.d $a0, 1\n"
+	// 	"add.d $a1, $zero, %0\n"
+	// 	"li.d $a2, %1\n"
+	// 	"li.d $a7, 64\n"
+	// 	"syscall 0"
+	// 	:: "r"( str ),
+	// 	"i"( sizeof( str ) )
+	// 	);
+	write( 1, str, sizeof( str ) );
 	while ( 1 );
 }
