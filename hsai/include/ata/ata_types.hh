764e2c2e16242a25d8067820c018b3577410f391
LiShuang <lishuang.meng@qq.com>
2024-07-17 00:51:43 +0800

    [hsai] 添加 SATA/AHCI 驱动相关定义； [qemu2k1000] 添加PCI相关定义；

---
+++ hsai/include/ata/ata_types.hh
@@ -0,0 +1,21 @@
+//
+// Created by Li Shuang ( pseudonym ) on 2024-07-17 
+// --------------------------------------------------------------
+// | Note: This code file just for study, not for commercial use 
+// | Contact Author: lishuang.mk@whu.edu.cn 
+// --------------------------------------------------------------
+//
+
+#pragma once
+
+namespace hsai
+{
+	// 这个文件用于基本类型定义，提高可读性
+	// 此处的word类型字长16位是延续ATA协议的定义
+
+	using byte = uint8;
+	using word = uint16;
+	using dword = uint32;
+	using qword = uint64;
+
+} // namespace hsai
