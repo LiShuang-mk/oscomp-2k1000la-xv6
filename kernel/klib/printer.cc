cb2ad7f2a8db945fc686f4aeb2d92e1da7b16e14
LiShuang <lishuang.meng@qq.com>
2024-07-27 18:20:24 +0800

    [printer] 修改printer算法； printer算法支持更多格式化功能： 1. 指定最小宽度；2. 左对齐；3. 空格填充到指定宽度；4. 使用0填充；5. 可附带前缀(0x,0,0b)；6. 按4位数分隔显示；7. 可使用h,l,ll,L指定字长；8. 可强制显示正号；

--- kernel/klib/printer.cc
+++ kernel/klib/printer.cc
@@ -7,7 +7,7 @@
 //
 
 #include "klib/printer.hh"
-#include "klib/common.hh"
+#include "klib/klib.hh"
 
 #include <hsai_log.hh>
 #include <hsai_global.hh>
@@ -19,7 +19,8 @@ namespace klib
 	Printer k_printer;
 
 	int Printer::_trace_flag = 0;
-	char Printer::_digits[] = "0123456789abcdef";
+	char Printer::_lower_digits[] = "0123456789abcdef";
+	char Printer::_upper_digits[] = "0123456789ABCDEF";
 
 	void Printer::init( const char *name )
 	{
@@ -32,10 +33,12 @@ namespace klib
 			while ( 1 );
 		if ( !_console->support_stream() )
 			while ( 1 );
-		
+
 		_lock.init( name );
 		_locking = 1;
 		_panicked = 0;
+
+		new ( &_buffer ) PrinterBuffer( _internal_buffer, printer_buffer_size );
 
 		hsai::p_hsai_logout = &level_log_out;
 		hsai::p_hsai_assert = &assert_log_out;
@@ -56,7 +59,7 @@ namespace klib
 		i = 0;
 		do
 		{
-			buf[ i++ ] = _digits[ x % base ];
+			buf[ i++ ] = _lower_digits[ x % base ];
 		}
 		while ( ( x /= base ) != 0 );
 
@@ -69,13 +72,13 @@ namespace klib
 	}
 
 
-	void Printer::printbyte( uchar x )
+	void Printer::printbyte( uint8 x )
 	{
 		char buf[ 2 ];
 
-		buf[ 0 ] = _digits[ x & 0xFU ];
+		buf[ 0 ] = _lower_digits[ x & 0xFU ];
 		x >>= 4;
-		buf[ 1 ] = _digits[ x & 0xFU ];
+		buf[ 1 ] = _lower_digits[ x & 0xFU ];
 
 		if ( _type == out_type::console && _console )
 		{
@@ -92,7 +95,292 @@ namespace klib
 		_console->put_char_sync( '0' );
 		_console->put_char_sync( 'x' );
 		for ( i = 0; i < ( sizeof( uint64 ) * 2 ); i++, x <<= 4 )
-			_console->put_char_sync( _digits[ x >> ( sizeof( uint64 ) * 8 - 4 ) ] );
+			_console->put_char_sync( _lower_digits[ x >> ( sizeof( uint64 ) * 8 - 4 ) ] );
+	}
+
+	void Printer::print_number( PrinterBuffer &buf, ulong num, int base, print_flag flag, char qualifier, int width )
+	{
+		char tmp[ 64 ] = { 0 };
+		int tmpi = 0;
+		char *digits = nullptr;
+
+		int space_pad;		// to calculate the space padding length
+		int num_pad;		// to calculate the zero padding length
+		space_pad = num_pad = 0;
+
+		// zero padding is confict with left-aign
+
+		if ( flag.left == 1 ) flag.zero_pad = 0;
+
+		// check if use upper-case letter
+
+		digits = ( flag.upper == 1 ) ? _upper_digits : _lower_digits;
+
+		// check sign
+
+		char sign = 0;
+		if ( flag.sign == 1 )
+		{
+			switch ( qualifier )
+			{
+				case 'h': if ( ( i16 ) num < 0 ) { sign = '-'; num = ( u16 ) -num; } break;
+				case 'L':
+				case 'l': if ( ( i64 ) num < 0 ) { sign = '-'; num = ( u64 ) -num; } break;
+				default:  if ( ( i32 ) num < 0 ) { sign = '-'; num = ( u32 ) -num; } break;
+			}
+			if ( sign != '-' )
+			{
+				if ( flag.plus == 1 ) sign = '+';
+				else if ( flag.space == 1 ) sign = ' ';
+			}
+		}
+
+		space_pad = width;
+
+		// push the number bit into tmp
+
+		if ( num == 0 ) { tmp[ tmpi ] = '0'; ++tmpi; }
+		else for ( ; num != 0; tmpi++ ) tmp[ tmpi ] = digits[ _divide( num, base ) ];
+
+		space_pad -= tmpi;
+
+		// seperation should occupy room
+
+		int sepr_cnt = 0;
+		if ( flag.seperate == 1 )
+			sepr_cnt = ( tmpi - 1 ) / 4;
+		space_pad -= sepr_cnt;
+
+		// prefix should occupy room
+
+		if ( flag.prefix == 1 )
+		{
+			if ( base == 2 || base == 16 )
+				space_pad -= 2;
+			else if ( base == 8 )
+				space_pad -= 1;
+		}
+
+		// the sign should occupy room
+
+		if ( sign != 0 )
+			space_pad--;
+
+		// if use zero-pad, move space_pad to num_pad
+
+		if ( flag.zero_pad == 1 )
+		{
+			num_pad = space_pad;
+			space_pad = 0;
+		}
+
+		// to pad if not left-align
+
+		if ( flag.left == 0 )
+			for ( ; space_pad > 0; space_pad-- )
+				buf.put( ' ' );
+
+		// put the sign
+
+		if ( sign != 0 )
+			buf.put( sign );
+
+		// put the prefix 
+
+		if ( flag.prefix == 1 )
+		{
+			if ( base == 2 ) { buf.put( '0' ); buf.put( 'b' ); }
+			else if ( base == 8 ) { buf.put( '0' ); }
+			else if ( base == 16 ) { buf.put( '0' ); buf.put( 'x' ); }
+		}
+
+		// to pad zero-num
+
+		for ( ; num_pad > 0; num_pad--, tmpi++ )
+			tmp[ tmpi ] = '0';
+
+		// to put number in tmp
+
+		tmpi--;
+		if ( ( tmpi % 4 ) == 3 )			// remove '_' in the front
+			buf.put( tmp[ tmpi ] );
+		tmpi--;
+		if ( flag.seperate == 1 )
+		{
+			for ( ; tmpi >= 0; tmpi-- )
+			{
+				if ( ( tmpi % 4 ) == 3 )
+					buf.put( '_' );
+				buf.put( tmp[ tmpi ] );
+			}
+		}
+		else for ( ; tmpi >= 0; tmpi-- )
+			buf.put( tmp[ tmpi ] );
+
+		// if left-align, it would be space_pad > 0 here
+
+		for ( ; space_pad > 0; space_pad-- )
+			buf.put( ' ' );
+	}
+
+	void Printer::vbprintf( PrinterBuffer &buf, const char *fmt, va_list args )
+	{
+		if ( buf.size() <= 0 ) return;
+
+		for ( ; *fmt != 0; ++fmt )
+		{
+			// put a char
+			if ( *fmt != '%' )
+			{
+				if ( _trace_flag == 1 && *fmt == '\n' )
+				{
+					const char * lb = "\n\t     ";
+					for ( ; *lb != 0; ++lb )
+						buf.put( *lb );
+				}
+				else buf.put( *fmt );
+				continue;
+			}
+
+
+			// collect the flag
+			print_flag fl = ( print_flag ) 0;
+
+			for ( bool get_fl = true; get_fl; )
+			{
+				++fmt;				// skip the first '%'
+				switch ( *fmt )
+				{
+					case '-': fl.left = 1; break;
+					case '+': fl.plus = 1; break;
+					case ' ': fl.space = 1; break;
+					case '#': fl.prefix = 1; break;
+					case '0': fl.zero_pad = 1; break;
+					case '_': fl.seperate = 1; break;
+					default: get_fl = false; break;
+				}
+			}
+
+			// collect the width
+			int width = -1;
+			if ( _is_number( *fmt ) )
+			{
+				int i = 0;
+				for ( ; _is_number( *fmt ); fmt++ )
+					i = i * 10 + _to_number( *fmt );
+				width = i;
+			}
+			else if ( *fmt == '*' )		// the width is in the args
+			{
+				++fmt;
+				width = va_arg( args, int );
+				if ( width < 0 )
+				{
+					width = -width;
+					fl.left = 1;
+				}
+			}
+
+			// collect the qualifier
+			char qualifier = 0;
+			if ( *fmt == 'l' || *fmt == 'L' || *fmt == 'h' )
+			{
+				qualifier = *fmt;
+				fmt++;
+				if ( qualifier == 'l' && *fmt == 'l' )
+				{
+					qualifier = 'L';
+					++fmt;
+				}
+			}
+
+			// collect the base, or put directly if format is 'c','s','p','%'
+			int base = 10;
+			char ch;
+			const char * str = nullptr;
+			int str_len;
+			switch ( *fmt )
+			{
+				case 'c':
+					if ( fl.left == 0 )					// padding space
+						for ( ; width > 1; width-- )	// leave one room to contain the char
+							buf.put( ' ' );
+					ch = ( char ) va_arg( args, int );
+					buf.put( ch );
+					width--;
+					for ( ; width > 0; width-- )
+						buf.put( ' ' );					// padding space
+					continue;
+
+				case 's':
+					str = va_arg( args, const char * );
+					if ( str == nullptr ) str = "(NULL)";
+					str_len = ( int ) strlen( str );
+					str_len = str_len > width ? width : str_len;
+
+					if ( fl.left == 1 )
+						for ( ; str_len < width; width-- )		// padding space
+							buf.put( ' ' );
+
+					for ( int i = 0; i < str_len; ++i )
+						buf.put( str[ i ] );
+
+					for ( ; str_len < width; width-- )			// padding space
+						buf.put( ' ' );
+
+					continue;
+
+				case 'p':
+					fl.prefix = 1;
+					fl.zero_pad = 1;
+					if ( width < 0 ) width = sizeof( void * ) << 1;
+					width += 2;		// prefix
+					print_number( buf, ( ulong ) va_arg( args, void * ),
+						16, fl, qualifier, width );
+					continue;
+
+				case '%':
+					buf.put( '%' );
+					continue;
+
+				case 'b': base = 2; break;
+				case 'o': base = 8; break;
+				case 'B': qualifier = 'B';
+				case 'X': fl.upper = 1;
+				case 'x': base = 16; break;
+				case 'd':
+				case 'i': fl.sign = 1; break;
+				case 'u': break;
+
+				default:
+					const char * idx = fmt;
+					do --fmt; while ( *fmt != '%' );
+					for ( ; fmt < idx; fmt++ )
+						buf.put( *fmt );
+					continue;
+			}
+
+			// print number
+			ulong num;
+
+			if ( qualifier == 'l' || qualifier == 'L' )
+				num = va_arg( args, ulong );
+			else if ( qualifier == 'h' )
+			{
+				num = ( u16 ) va_arg( args, uint );
+				if ( fl.sign )
+					num = ( i16 ) num;
+			}
+			else if ( qualifier == 'B' )
+			{
+				num = ( u8 ) va_arg( args, uint );
+			}
+			else
+				num = va_arg( args, uint );
+
+			print_number( buf, num, base, fl, qualifier, width );
+
+		}
 	}
 
 	void Printer::printf( const char *fmt, ... )
@@ -105,9 +393,9 @@ namespace klib
 
 	void Printer::vprintf( const char *fmt, va_list ap )
 	{
-		int i, c;
 		int locking;
-		char *s;
+
+		_buffer.clear();
 
 		locking = _locking;
 		if ( locking )
@@ -116,71 +404,13 @@ namespace klib
 		if ( fmt == 0 )
 			log_panic( "null fmt" );
 
-		for ( i = 0; ( c = fmt[ i ] & 0xff ) != 0; i++ )
-		{
-			if ( c != '%' )
-			{
-				if ( _type == out_type::console && _console )
-				{
-					if ( _trace_flag == 1 && c == '\n' )
-					{
-						_locking = 0;
-						_trace_flag = 0;
-						printf( "\n\t     " );
-						_trace_flag = 1;
-						_locking = locking;
-					}
-					else
-					{
-						_console->put_char_sync( c );
-					}
-				}
-				continue;
-			}
-			c = fmt[ ++i ] & 0xff;
-			if ( c == 0 )
-				break;
-			switch ( c )
-			{
-				case 'd':
-					printint( va_arg( ap, long ), 10, 1 );
-					break;
-				case 'u':
-					printint( va_arg( ap, long ), 10, 0 );
-					break;
-				case 'x':
-					printint( va_arg( ap, long ), 16, 0 );
-					break;
-				case 'p':
-					printptr( va_arg( ap, uint64 ) );
-					break;
-				case 's':
-					if ( ( s = va_arg( ap, char* ) ) == 0 )
-						s = ( char * ) "(null)";
-					if ( _type == out_type::console && _console )
-						for ( ; *s; s++ )
-							_console->put_char_sync( *s );
-					break;
-				case 'B':
-					printbyte( ( uchar ) va_arg( ap, int ) );
-					break;
-				case '%':
-					if ( _type == out_type::console && _console )
-						_console->put_char_sync( '%' );
-					break;
-				default:
-				  // Print unknown % sequence to draw attention.
-					if ( _type == out_type::console && _console )
-					{
-						_console->put_char_sync( '%' );
-						_console->put_char_sync( c );
-					}
-					break;
-			}
-		}
+		vbprintf( _buffer, fmt, ap );
 
 		if ( locking )
 			_lock.release();
+
+		if ( _console != nullptr )
+			_console->write( ( void * ) _buffer.get_content(), _buffer.content_length() );
 	}
 
 	void Printer::panic( const char *f, uint l, const char *info, ... )
