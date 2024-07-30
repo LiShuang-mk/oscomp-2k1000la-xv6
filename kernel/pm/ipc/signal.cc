246d1c8234110ee08bebcc5855349e1ec7efa3f3
Sun <1533569296@qq.com>
2024-07-30 21:46:54 +0800

    [ fix syscall ] 134 sigAction

--- kernel/pm/ipc/signal.cc
+++ kernel/pm/ipc/signal.cc
@@ -13,9 +13,9 @@ namespace pm
                     return -1; 
                 pm::Pcb * cur_proc = pm::k_pm.get_cur_pcb();
                 if( oldact != nullptr )
-                    oldact = cur_proc->_sigactions[ flag - 1 ];
+                    *oldact = *( cur_proc->_sigactions[ flag - 1 ] );
                 if( newact != nullptr )
-                    cur_proc->_sigactions[ flag - 1] = newact;
+                    cur_proc->_sigactions[ flag - 1 ] = newact;
                 return 0;
             }
         }
--- kernel/syscall/syscall_handler.cc
+++ kernel/syscall/syscall_handler.cc
@@ -1017,9 +1017,9 @@ namespace syscall
 	{
 		pm::Pcb *proc = pm::k_pm.get_cur_pcb();
 		[[maybe_unused]]mm::PageTable *pt = proc->get_pagetable();
-		[[maybe_unused]]pm::ipc::signal::sigaction *a_newact,* a_oldact;
-		a_newact = nullptr;
-		a_oldact = nullptr;
+		[[maybe_unused]]pm::ipc::signal::sigaction a_newact, a_oldact;
+		// a_newact = nullptr;
+		// a_oldact = nullptr;
 		uint64 newactaddr, oldactaddr;
 		int flag;
 		int ret = -1;
@@ -1033,21 +1033,23 @@ namespace syscall
 		if( _arg_addr( 2, oldactaddr ) < 0 )
 			return -1;
 
-		if( oldactaddr == 0 )
-			a_oldact = nullptr;
+		if( oldactaddr != 0 )
+			a_oldact =  pm::ipc::signal::sigaction();
+
 		if( newactaddr != 0 )
 		{
 			if( mm::k_vmm.copy_in( *pt, &a_newact, newactaddr, sizeof(pm::ipc::signal::sigaction ) ) < 0 )
 		 		return -1;
-			ret = pm::ipc::signal::sigAction( flag, a_newact, a_oldact );
+			//a_newact = ( pm::ipc::signal::sigaction *)(hsai::k_mem->to_vir( pt->walk_addr( newactaddr ) ));
+			ret = pm::ipc::signal::sigAction( flag, &a_newact, nullptr );
 		}
 		else
 		{
-			ret = pm::ipc::signal::sigAction( flag, a_newact, a_oldact );
+			ret = pm::ipc::signal::sigAction( flag, &a_newact, &a_oldact );
 		}
 		if( ret == 0 && oldactaddr != 0 )
 		{
-			if( mm::k_vmm.copyout( *pt, oldactaddr, a_oldact, sizeof(pm::ipc::signal::sigaction ) ) < 0 )
+			if( mm::k_vmm.copyout( *pt, oldactaddr, &a_oldact, sizeof(pm::ipc::signal::sigaction ) ) < 0 )
 				return -1;
 		}
 		return ret;
